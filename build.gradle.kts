41842db31d1176637c81fe31e9baaca902b52ef9
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2024-01-16 16:49:29 +0600

    "responsive_ui" branch created

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,8 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
 
-
 }
-
+//
 
 
 true // Needed to make the Suppress annotation work for the plugins block
